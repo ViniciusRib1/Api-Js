e78f89c0e744541e0cb1f79fbda0321f593f13c2
Vinicius Ribeiro <viniciusribeiro0709@gmail.com>
2024-11-19 20:05:51 -0300

    Colocando mais coisas

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 import express from "express";
 
+// Uma lista que são os [] que possue objetos que são os {}
+const posts = [
+    {
+      id: 1, 
+      descricao: "Uma foto teste",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 2, 
+      descricao: "Um lindo pôr do sol na praia",
+      imagem: "https://placeimg.com/640/480/nature"
+    },
+    {
+      id: 3, 
+      descricao: "Receita de bolo de chocolate irresistível",
+      imagem: "https://picsum.photos/id/237/300/200"
+    },
+  ];
+
+// Guardar o servidor em uma variável app
+const app = express(); 
+app.use(express.json());
 
 
-const app = express(); // Guardar o servidor em uma variável app
 // Fala para o app que está o servidor do express ouvir a porta 3000 
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log("Servidor escutando em http://localhost:3000/api");
 });
 
-app.get("/BomDia", (req, res) => {
-    res.status(200).send("Boas Vindas á api legalzinha kk");
+
+app.get("/api", (req, res) => {
+    res.status(200).send(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    });
+};
+
+app.get("/api/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
